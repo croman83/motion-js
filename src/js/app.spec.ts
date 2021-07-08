e35acd55afcda2ee500f12753d9977beee5d9c09
Roman <casian.roma@gmail.com>
2021-07-08 14:37:03 +0200

    boilerplate

---
+++ src/js/app.spec.ts
@@ -0,0 +1,9 @@
+describe("test add function", () => {
+  it("should return 15 for add(10,5)", () => {
+    expect(5+10).toBe(15);
+  });
+  it("should return 5 for add(2,3)", () => {
+    expect(3+2).toBe(5);
+  });
+});
+
